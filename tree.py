6881f95db8119d469254ad62eb9e6373d950b582
Mordechai Fast <mordechai.fast@gmail.com>
2023-03-09 20:22:40 +0200

    Differentiate last item

--- tree.py
+++ tree.py
@@ -4,15 +4,20 @@ __version__ = '0.0.3'
 from pathlib import Path
 
 TEE          = "├──"
+ELBOW        = "└──"
 PIPE_PREFIX  = "│   "
+SPACE_PREFIX = "    "
 
 def print_dir(directory: Path, indent=""):
     # Hide hidden items by default
     items = [item for item in directory.iterdir() if item.name[0] != '.']
-    for item in items:
-        print(f"{indent}{TEE} {item.name}")
+    num_of_items = len(items)
+    for index, item in enumerate(items):
+        last = (index == num_of_items - 1)
+        print(f"{indent}{ELBOW if last else TEE} {item.name}")
         if item.is_dir():
-            print_dir(item, indent=indent+PIPE_PREFIX)
+            print_dir(item, 
+             indent= indent + (SPACE_PREFIX if last else PIPE_PREFIX))
 
 print('.')
 print_dir(Path.cwd())
