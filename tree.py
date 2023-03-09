758942582ce5874b7f7d2dbe6db6562f44ac9f6e
Mordechai Fast <mordechai.fast@gmail.com>
2023-03-09 23:51:12 +0200

    count

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 """A clone of the Linux command 'tree'"""
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from pathlib import Path
 
@@ -8,7 +8,11 @@ ELBOW        = "└──"
 PIPE_PREFIX  = "│   "
 SPACE_PREFIX = "    "
 
+dir_count, file_count = 0, 0
+
 def print_dir(directory: Path, indent=""):
+    # Use the total count of directories and files
+    global dir_count, file_count
     # Hide hidden items by default
     items = [item for item in directory.iterdir() if item.name[0] != '.']
     # Alphabetize
@@ -20,9 +24,12 @@ def print_dir(directory: Path, indent=""):
         print(f"{indent}{ELBOW if last else TEE} {item.name}")
         # Print directories recursivly
         if item.is_dir():
+            dir_count += 1
             print_dir(item, 
              indent= indent + (SPACE_PREFIX if last else PIPE_PREFIX))
+        elif item.is_file():
+            file_count += 1
 
 print('.')
 print_dir(Path.cwd())
-print()
+print(f'\n{dir_count} directories, {file_count} files')
