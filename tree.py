e7bf89ccb193b77b93072cc6fc67707b2de4524c
Mordechai Fast <mordechai.fast@gmail.com>
2023-03-11 23:41:29 +0200

    color

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 """A clone of the Linux command 'tree'"""
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from pathlib import Path
 
@@ -9,6 +9,10 @@ PIPE_PREFIX  = "│   "
 SPACE_PREFIX = "    "
 
 dir_count, file_count = 0, 0
+
+def color(text) -> str:
+    """Returns the object's string in bold blue"""
+    return f'\033[94m\033[1m{str(text)}\033[0m'
 
 def print_dir(directory: Path, indent=""):
     # Use the total count of directories and files
@@ -21,15 +25,16 @@ def print_dir(directory: Path, indent=""):
     num_of_items = len(items)
     for index, item in enumerate(items):
         last = (index == num_of_items - 1)
-        print(f"{indent}{ELBOW if last else TEE} {item.name}")
         # Print directories recursivly
         if item.is_dir():
             dir_count += 1
+            print(f"{indent}{ELBOW if last else TEE} {color(item.name)}")
             print_dir(item, 
              indent= indent + (SPACE_PREFIX if last else PIPE_PREFIX))
         elif item.is_file():
             file_count += 1
+            print(f"{indent}{ELBOW if last else TEE} {item.name}")
 
-print('.')
+print(color('.'))
 print_dir(Path.cwd())
 print(f'\n{dir_count} directories, {file_count} files')
